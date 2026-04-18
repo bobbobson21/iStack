c13a0324e1ef5ebfe3bac3bddd021df9c7597dc0
kyle edwards <126681707+bobbobson21@users.noreply.github.com>
2026-04-18 13:45:47 +0100

    added comment support

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -15,9 +15,9 @@
 
 #include "istack/istack.h"
 
-#define COMPILE_MODE__FILE_UNCOMPILED 1 //compiles sourcew code files //istf
+#define COMPILE_MODE__FILE_UNCOMPILED 0 //compiles sourcew code files //istf
 #define COMPILE_MODE__COMMAND_LINE 0 //runs command line code //istcmd
-#define COMPILE_MODE__FILE_COMPILE 0 //compiles a file for faster exacution //istfc
+#define COMPILE_MODE__FILE_COMPILE 1 //compiles a file for faster exacution //istfc
 #define COMPILE_MODE__FILE_LOAD 0 //runs a compiled file //istfe
 
 //argument parsing has to be handled in compiler
@@ -144,24 +144,27 @@ namespace compiler
 		while (fileReader.good() == true && fileReader.good() == true && fileReader.eof() == false)
 		{
 			std::string parserLine = "";
-
 			std::getline(fileReader, parserLine);
-			(*parser) << parserLine.c_str();
 
-			if (parser->InputBufferOverflowed() == true)
+			if (parserLine.substr(0, 2) != "//")
 			{
-				std::cout << "error: parsing buffer of size (1024) overflowed on line (starting from line 0): " << currentLineInParsing << std::endl;
+				(*parser) << parserLine.c_str();
 
-				fileReader.close();
-				return false;
-			}
+				if (parser->InputBufferOverflowed() == true)
+				{
+					std::cout << "error: parsing buffer of size (1024) overflowed on line (starting from line 0): " << currentLineInParsing << std::endl;
 
-			if (parser->IsParsingSucessful() == false)
-			{
-				std::cout << "error: parsing interpretation failure occoured on line (starting from line 0): " << currentLineInParsing << std::endl;
+					fileReader.close();
+					return false;
+				}
 
-				fileReader.close();
-				return false;
+				if (parser->IsParsingSucessful() == false)
+				{
+					std::cout << "error: parsing interpretation failure occoured on line (starting from line 0): " << currentLineInParsing << std::endl;
+
+					fileReader.close();
+					return false;
+				}
 			}
 
 			currentLineInParsing++;
@@ -226,40 +229,42 @@ namespace compiler
 			return false;
 		}
 
-		unsigned int currentLineInParsing = 0;
-
 		while (fileReader.good() == true && fileReader.good() == true && fileReader.eof() == false)
 		{
-			char charBuffer = ' ';
+			std::string parserLine = "";
+			std::getline(fileReader, parserLine);
 
-			fileReader.read(&charBuffer, 1);
-			(*parser) << charBuffer;
-
-			if (parser->InputBufferOverflowed() == true)
+			if (parserLine.substr(0, 2) != "//")
 			{
-				std::cout << "error: parsing buffer of size (1024) overflowed on line (starting from line 0): " << currentLineInParsing << std::endl;
+				for (size_t i = 0; i < parserLine.length(); i++)
+				{
+					(*parser) << parserLine[i];
 
-				fileReader.close();
-				fileWiter.close();
-				return false;
+					if (parser->InputBufferOverflowed() == true)
+					{
+						std::cout << "error: parsing buffer of size (1024) overflowed" << std::endl;
+
+						fileReader.close();
+						fileWiter.close();
+						return false;
+					}
+
+					if (parser->IsParsingSucessful() == false)
+					{
+						std::cout << "error: parsing interpretation failure occoured" << std::endl;
+
+						fileReader.close();
+						fileWiter.close();
+						return false;
+					}
+
+					if (codeFrame->Length() > 0)
+					{
+						fileWiter << codeFrame->Top().m_modualTypeCode << ",(" << n_lastAguments << ");";
+						codeFrame->Pop();
+					}
+				}
 			}
-
-			if (parser->IsParsingSucessful() == false)
-			{
-				std::cout << "error: parsing interpretation failure occoured on line (starting from line 0): " << currentLineInParsing << std::endl;
-
-				fileReader.close();
-				fileWiter.close();
-				return false;
-			}
-
-			if (codeFrame->Length() > 0)
-			{
-				fileWiter << codeFrame->Top().m_modualTypeCode << ",(" << n_lastAguments << ");";
-				codeFrame->Pop();
-			}
-
-			currentLineInParsing++;
 		}
 
 		fileReader.close();
